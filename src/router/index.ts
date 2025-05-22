b6da086bacffc3f089be59c6850d423651a77add
Adricalde10 <146762237+Adricalde10@users.noreply.github.com>
2025-05-22 18:57:43 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHashHistory } from '@ionic/vue-router'; // <-- CAMBIO AQUÍ
+import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue';
+import TabsPage from '../views/TabsPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -48,31 +48,30 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: '/register',
         name: 'Register',
-        component: () => import('@/views/Register.vue')
+        component: () => import('@/views/Register.vue') // Verifica que esta ruta sea correcta
       },
       {
         path: '/login',
         name: 'Login',
-        component: () => import('@/views/Login.vue')
+        component: () => import('@/views/login.vue') // Verifica que esta ruta sea correcta
       },
       {
         path: '/MoviesPage',
         name: 'MoviesPage',
-        component: () => import('@/views/MoviesPage.vue')
+        component: () => import('@/views/MoviesPage.vue') // Verifica que esta ruta sea correcta
       },
       {
         path: '/ManageContent',
         name: 'ManageContent',
-        component: () => import('@/views/ManageContent.vue')
+        component: () => import('@/views/ManageContent.vue') // Verifica que esta ruta sea correcta
       },
     ]
   }
-];
+]
 
 const router = createRouter({
-  history: createWebHashHistory(), // <-- USAR HASH MODE PARA ANDROID
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
-});
+})
 
-export default router;
-  
+export default router
